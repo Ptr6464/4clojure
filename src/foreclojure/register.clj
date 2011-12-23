bc2a1d944625d75cf384fda0319659d7d115ca2f
Anthony Grimes <i@raynes.me>
2011-12-23 08:02:57 -0600

    Move register to noir.

--- src/foreclojure/register.clj
+++ src/foreclojure/register.clj
@@ -1,28 +1,29 @@
 (ns foreclojure.register
-  (:require [sandbar.stateful-session :as   session]
-            [ring.util.response       :as   response])
+  (:require [noir.session             :as   session]
+            [noir.response            :as   response])
   (:import  [org.jasypt.util.password StrongPasswordEncryptor])
   (:use     [hiccup.form-helpers      :only [form-to text-field password-field]]
-            [compojure.core           :only [defroutes GET POST]]
             [foreclojure.utils        :only [form-row assuming flash-error plausible-email?]]
-            [foreclojure.template     :only [def-page]]
+            [foreclojure.template     :only [html-doc]]
             [foreclojure.messages     :only [err-msg]]
-            [somnium.congomongo       :only [insert! fetch-one]]))
+            [somnium.congomongo       :only [insert! fetch-one]]
+            [noir.core                :only [defpage]]))
 
-(def-page register-page []
-  {:title "4clojure &ndash; Register"
-   :content
-   (list
-    [:div.error (session/flash-get :error)]
-    (form-to [:post "/register"]
-      [:table
-       (map form-row
-            [[text-field :user "Username (4-13 chars.)" (session/flash-get :user)]
-             [password-field :pwd "Password (7+ chars.)"]
-             [password-field :repeat-pwd "Repeat Password"]
-             [text-field :email "Email" (session/flash-get :email)]])
-       [:tr
-        [:td [:button {:type "submit"} "Register"]]]]))})
+(defn register-page []
+  (html-doc
+   {:title "4clojure &ndash; Register"
+    :content
+    (list
+     [:div.error (session/flash-get :error)]
+     (form-to [:post "/register"]
+              [:table
+               (map form-row
+                    [[text-field :user "Username (4-13 chars.)" (session/get :user)]
+                     [password-field :pwd "Password (7+ chars.)"]
+                     [password-field :repeat-pwd "Repeat Password"]
+                     [text-field :email "Email" (session/get :email)]])
+               [:tr
+                [:td [:button {:type "submit"} "Register"]]]]))}))
 
 (defn do-register [user pwd repeat-pwd email]
   (let [lower-user (.toLowerCase user)]
@@ -46,14 +47,15 @@
                  {:user lower-user
                   :pwd (.encryptPassword (StrongPasswordEncryptor.) pwd)
                   :email email})
-        (session/session-put! :user lower-user)
+        (session/put! :user lower-user)
         (response/redirect "/"))
       (do
         (session/flash-put! :user user)
         (session/flash-put! :email email)
         (flash-error "/register" why)))))
 
-(defroutes register-routes
-  (GET  "/register" [] (register-page))
-  (POST "/register" {{:strs [user pwd repeat-pwd email]} :form-params}
-        (do-register user pwd repeat-pwd email)))
+(defpage "/register" []
+  (register-page))
+
+(defpage [:post "/register"] {:keys [user pwd repeat-pwd email]}
+  (do-register user pwd repeat-pwd email))
